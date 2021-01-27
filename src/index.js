78d4579d9aa9376ed0943e9d90218b557b0d28d1
AlexStepanchuk <666stepashka666@gmail.com>
2021-01-27 23:42:46 +0300

    solve

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (arguments.length===0) {return 0}
+  if (array.length===0) {return 0}
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (arguments.length===0) {return 0}
+  if (array.length===0) {return 0}
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (arguments.length===0) {return 0}
+  if (array.length===0) {return 0}
+  let sum = 0;
+  array.forEach(element => {
+    sum = sum + element
+  });
+  return sum/(array.length);
 }
